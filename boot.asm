f0f405667ea476ed585676b7b85b06b8d7fe1cca
操作系统学习 <13252630718@163.com>
2024-01-01 02:46:01 -0800

    boot

--- boot.asm
+++ boot.asm
@@ -5,7 +5,7 @@ OffsetOfLoader equ 0x00
 
 RootDirSectors equ 14
 SectorNumOfRootDirStart equ 19
-SectorNumOfFAT!Start equ 1
+SectorNumOfFAT1Start equ 1
 SectorBalance equ 17
 
     jmp short Label_Start
@@ -65,6 +65,7 @@ Label_Start:
     mov bp, StartBootMessage
     int 10h
 
+
 ;=======软盘驱动器复位
 
     xor ah, ah
@@ -72,7 +73,198 @@ Label_Start:
     int 13h
 
     jmp $
-StartBootMessage: db "yuanshen!? qidong!"  
+;======= search loader.bin
+    mov word [SectorNo], SectorNumOfRootDirStart
+
+Label_Search_In_Root_Dir_Begin:
+
+    cmp word [RootDirSizeForLoop], 0
+    jz Label_No_LoaderBin
+    dec word [RootDirSizeForLoop]
+    mov ax, 00h
+    mov es, ax
+    mov bx, 8000h
+    mov ax, [SectorNo]
+    mov cl, 1
+    call Func_ReadOneSector
+    mov si, LoaderFileName
+    mov di, 8000h
+    cld
+    mov dx, 10h
+
+Label_Search_For_LoaderBin:
+
+    cmp dx, 0
+    jz Label_Goto_Next_Sector_In_Root_Dir
+    dec dx
+    mov cx, 11
+
+Label_Cmp_FileName:
+
+    cmp cx, 0
+    jz Label_Filename_Found
+    dec cx
+    lodsb
+    cmp al, byte [es:di]
+    jz Label_Go_On
+    jmp Label_Different
+
+Label_Go_On:
+
+    inc di
+    jmp Label_Cmp_FileName
+
+Label_Different:
+
+    and di, 0ffe0h
+    add di, 20h
+    mov si, LoaderFileName
+    jmp Label_Search_For_LoaderBin
+
+Label_Goto_Next_Sector_In_Root_Dir:
+
+    and word [SectorNo], 1
+    jmp Label_Search_In_Root_Dir_Begin
+    
+;============== display on screen : ERROR:No LOADER Found
+
+Label_No_LoaderBin:
+
+    mov ax, 1301h
+    mov bx, 008ch
+    mov dx, 0100h
+    mov cx, 21
+    push ax
+    mov ax, ds
+    mov es, ax
+    pop ax
+    mov bp, NoLoaderMessage
+    int 10h
+    jmp $
+
+;========== get FAT Entry
+
+Func_GetFATEntry:
+
+    push es
+    push bx
+    push ax
+    mov ax, 00
+    mov es, ax
+    pop ax
+    mov byte [Odd], 0
+    mov bx, 3
+    mul bx
+    mov bx
+    div bx
+    cmp dx, 0
+    jz Label_Even
+    mov byte [Odd], 1
+
+Label_Even:
+
+    xor dx, dx
+    mov bx, [BPB_BytesPerSec]
+    div bx
+    push dx 
+    mov bx, 8000h
+    add ax, SectorNumOfFAT1Start
+    mov cl, 2
+    call Func_ReadOneSector
+
+    pop dx
+    add bx, dx
+    mov ax, [es:di]
+    cmp byte [Odd], 1
+    jnz Label_Even2
+
+Label_Even2:
+
+    add ax, 0fffh
+    pop bx
+    pop es
+    ret
+
+;================= found loader.bin name in root director struct
+
+Label_Filename_Found:
+
+    mov ax, RootDirSectors
+    and di, 0ffe0h
+    add di, 01ah
+    mov cx, word [es:di]
+    push cx
+    add cx, ax
+    add cx, SectorBalance
+    mov ax, BaseOfLoader
+    mov es, ax
+    mov bx, OffsetOfLoader
+    mov ax, cx
+
+Label_Go_On_Loading_File:
+
+    push ax
+    push bx
+    mov ah, 0eh
+    mov al, '.'
+    mov bl, 0fh
+    int 10h
+    pop bx
+    pop ax
+
+    mov cl, 1
+    call Func_ReadOneSector
+    pop ax
+    call Func_GetFATEntry
+    cmp ax, 0fffh
+    jz label_File_Loaded
+    push ax
+    mov dx, RootDirSectors
+    add ax, dx
+    add ax, SectorBalance
+    add bx, [BPB_BytesPerSec]
+    jmp Label_Go_On_Loading_File
+
+label_File_Loaded:
+
+    jmp $
+
+;======= read one sector from floppy
+Func_ReadOneSector:
+
+    push bp
+    mov bp, sp
+    sub esp, 2
+    mov byte [bp -2], cl
+    push bx
+    mov bl, [BPB_SecPerTrk]
+    div bl
+    inc ah
+    mov cl, ah
+    mov dh, al
+    shr al, 1
+    mov ch, al
+    and dh, 1
+    pop bx
+    mov dl, [BS_DrvNum]
+Label_Go_On_Reading:
+    mov ah, 2
+    mov al, byte [bp-2]
+    int 13h
+    jc Label_Go_On_Reading
+    add esp, 2
+    pop bp 
+    ret
+
+;======== tmp variable
+RootDirSizeForLoop dw RootDirSectors
+SectorNo dw 0
+Odd db 0
+
+;======== display meesges
+NoLoaderMessage: dw 0
+StartBootMessage: db "yuanshen!? qidong!"
+LoaderFileName: db "LOADER BIN",0  
 ;======用0填充本节
 
     times 510 - ($ - $$) db 0
